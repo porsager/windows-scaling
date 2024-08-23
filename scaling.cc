383125e30a4860047df8f503d502a744a7fe2b3e
Rasmus Porsager <rasmus@porsager.com>
2024-08-23 23:27:36 +0200

    Include index of display to change scaling for

--- scaling.cc
+++ scaling.cc
@@ -3,7 +3,6 @@
 
 #include <nan.h>
 #include <Windows.h>
-#include <wingdi.h>
 #include <vector>
 #include <string>
 #include "DpiHelper.h"
@@ -19,7 +18,7 @@ std::string FormatErrorMessage(const std::string& message, DWORD errorCode) {
 
 class DpiWrapper {
 public:
-    static UINT32 GetScaling() {
+    static UINT32 GetScaling(UINT32 displayIndex) {
         std::vector<DISPLAYCONFIG_PATH_INFO> paths;
         std::vector<DISPLAYCONFIG_MODE_INFO> modes;
 
@@ -27,19 +26,25 @@ public:
             Nan::ThrowError(FormatErrorMessage("Failed to retrieve display configuration paths and modes", GetLastError()).c_str());
         }
 
-        for (const auto& path : paths) {
-            if (path.flags & DISPLAYCONFIG_PATH_ACTIVE) {
-                auto dpiInfo = DpiHelper::GetDPIScalingInfo(path.sourceInfo.adapterId, path.sourceInfo.id);
-                if (dpiInfo.bInitDone) return dpiInfo.current;
-                Nan::ThrowError(FormatErrorMessage("Failed to retrieve DPI scaling information", GetLastError()).c_str());
-            }
+        if (displayIndex >= paths.size()) {
+            Nan::ThrowError("Display index out of bounds.");
+            return 0;
         }
 
-        Nan::ThrowError("No active display path found.");
+        LUID adapterID = paths[displayIndex].sourceInfo.adapterId;
+        UINT32 sourceID = paths[displayIndex].sourceInfo.id;
+
+        auto dpiInfo = DpiHelper::GetDPIScalingInfo(adapterID, sourceID);
+        if (dpiInfo.bInitDone) {
+            return dpiInfo.current;
+        } else {
+            Nan::ThrowError(FormatErrorMessage("Failed to retrieve DPI scaling information", GetLastError()).c_str());
+        }
+
         return 0;
     }
 
-    static bool SetScaling(UINT32 scaling) {
+    static bool SetScaling(UINT32 displayIndex, UINT32 scaling) {
         std::vector<DISPLAYCONFIG_PATH_INFO> paths;
         std::vector<DISPLAYCONFIG_MODE_INFO> modes;
 
@@ -47,30 +52,37 @@ public:
             Nan::ThrowError(FormatErrorMessage("Failed to retrieve display configuration paths and modes", GetLastError()).c_str());
         }
 
-        for (const auto& path : paths) {
-            if (path.flags & DISPLAYCONFIG_PATH_ACTIVE) {
-                if (DpiHelper::SetDPIScaling(path.sourceInfo.adapterId, path.sourceInfo.id, scaling)) return true;
-                Nan::ThrowError(FormatErrorMessage("Failed to set DPI scaling", GetLastError()).c_str());
-            }
+        if (displayIndex >= paths.size()) {
+            Nan::ThrowError("Display index out of bounds.");
+            return false;
         }
 
-        Nan::ThrowError("No active display path found.");
-        return false;
+        LUID adapterID = paths[displayIndex].sourceInfo.adapterId;
+        UINT32 sourceID = paths[displayIndex].sourceInfo.id;
+
+        return DpiHelper::SetDPIScaling(adapterID, sourceID, scaling);
     }
 };
 
 NAN_METHOD(GetScaling) {
-    info.GetReturnValue().Set(Nan::New(DpiWrapper::GetScaling()));
-}
-
-NAN_METHOD(SetScaling) {
     if (info.Length() < 1 || !info[0]->IsNumber()) {
-        Nan::ThrowTypeError("Scaling value must be a number.");
+        Nan::ThrowTypeError("Display index must be a number.");
         return;
     }
 
-    UINT32 scaling = Nan::To<UINT32>(info[0]).FromJust();
-    info.GetReturnValue().Set(Nan::New(DpiWrapper::SetScaling(scaling)));
+    UINT32 displayIndex = Nan::To<UINT32>(info[0]).FromJust();
+    info.GetReturnValue().Set(Nan::New(DpiWrapper::GetScaling(displayIndex)));
+}
+
+NAN_METHOD(SetScaling) {
+    if (info.Length() < 2 || !info[0]->IsNumber() || !info[1]->IsNumber()) {
+        Nan::ThrowTypeError("Display index and scaling value must be numbers.");
+        return;
+    }
+
+    UINT32 displayIndex = Nan::To<UINT32>(info[0]).FromJust();
+    UINT32 scaling = Nan::To<UINT32>(info[1]).FromJust();
+    info.GetReturnValue().Set(Nan::New(DpiWrapper::SetScaling(displayIndex, scaling)));
 }
 
 NAN_MODULE_INIT(Init) {
